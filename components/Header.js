7e29eb9de7ce2792f3fb6a3b0d9b6cec4222cda0
Emilien Hainaux <emilienhainaux@yahoo.fr>
2024-08-23 18:14:18 +0200

    first commit - create Next App + Header OK

---
+++ components/Header.js
@@ -0,0 +1,32 @@
+import React from "react";
+import styles from "../styles/Header.module.css";
+import Image from "next/image";
+import { FaSearch } from "react-icons/fa";
+
+export default function Header() {
+  return (
+    <div className={styles.mainContainer}>
+      <div className={styles.logoContainer}>
+        <Image
+          src="/images/GaminRetroLogo.png"
+          alt="Logo"
+          width={100}
+          height={100}
+          quality={100}
+        />
+      </div>
+      <div className={styles.titleContainer}>
+        <Image
+          src="/images/GaminRetroTitleSmall.png"
+          alt="Logo"
+          width={200}
+          height={100}
+          quality={100}
+        />
+      </div>
+      <div className={styles.searchIconContainer}>
+        <FaSearch style={styles.searchIcon} />
+      </div>
+    </div>
+  );
+}
