38030424a395f142236e149996c81b218d00b356
Emilien Hainaux <emilienhainaux@yahoo.fr>
2024-08-26 12:16:38 +0200

    feat recent games from db by 4

--- components/RecentGames.js
+++ components/RecentGames.js
@@ -1,11 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
 import styles from "../styles/RecentGames.module.css";
 
 export default function RecentGames() {
+  const [recentGames, setRecentGames] = useState([]);
+
+  useEffect(() => {
+    const fetchGames = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/games/recentgames");
+        if (!response.ok) {
+          throw new Error("Erreur lors de la récupération des jeux");
+        }
+        const data = await response.json();
+        setRecentGames(data);
+      } catch (error) {
+        console.error(error.message);
+      }
+    };
+    fetchGames();
+  }, []);
+
   return (
     <div className={styles.mainContainer}>
       <h1 className={styles.title}>Récemment ajoutés</h1>
-      <div className={styles.gamesContainer}></div>
+      <div className={styles.gamesContainer}>
+        {recentGames.map((game) => (
+          <div key={game._id} className={styles.gameCard}>
+            <Image
+              src={game.image}
+              alt={game.title}
+              width={100}
+              height={100}
+              layout="responsive"
+            />
+            <h3 className={styles.gameTitle}>{game.title}</h3>
+            <h4 className={styles.platformName}>{game.platform}</h4>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
