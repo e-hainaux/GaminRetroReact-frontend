b93937f3ed7525b307be1e05b4e284e5779439eb
Emilien Hainaux <emilienhainaux@yahoo.fr>
2024-08-26 16:52:20 +0200

    change next config mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: [],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/dbsqzf8cm/**",
+      },
+    ],
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
