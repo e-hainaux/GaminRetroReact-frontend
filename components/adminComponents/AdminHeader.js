9a0e8b7859b98613835899f4d749f0126b963c67
Emilien Hainaux <emilienhainaux@yahoo.fr>
2024-08-29 18:12:31 +0200

    feat login logout admin > just router OK / To do : auth

--- components/adminComponents/AdminHeader.js
+++ components/adminComponents/AdminHeader.js
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import { useRouter } from "next/router";
 import styles from "../../styles/AdminHeader.module.css";
 import Image from "next/image";
+import { FaPowerOff } from "react-icons/fa";
 
 export default function AdminHeader() {
+  const router = useRouter();
+  const [logged, setLogged] = useState(true);
+
+  const logout = () => {
+    console.log("Log out !");
+    router.push("/admin");
+  };
   return (
     <div className={styles.mainContainer}>
       <div className={styles.logoContainer}>
@@ -25,6 +34,11 @@ export default function AdminHeader() {
           className={styles.titleImage}
           priority={true}
         />
+      </div>
+      <div onClick={logout} className={styles.logoutButton}>
+        <div className={styles.logoutIconContainer}>
+          <FaPowerOff style={styles.logoutIcon} />
+        </div>
       </div>
     </div>
   );
